d9893b1c2a489704177f11c02f3ec2ce804479ad
yusufusman <yusuf.usman1003@gmail.com>
2025-06-17 14:47:58 +0100

    new task upload

--- src/main/java/org/ExpenseTracker/data/repositories/UserRepository.java
+++ src/main/java/org/ExpenseTracker/data/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.ExpenseTracker.data.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
+//    User findByUsername(String username);
     User findByEmail(String email);
 }
