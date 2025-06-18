35b6bec8881b171cec4e3d65e1a7731f9edf0e59
yusufusman <yusuf.usman1003@gmail.com>
2025-06-18 08:45:28 +0100

    new task upload

--- src/main/java/org/ExpenseTracker/config/WebConfig.java
+++ src/main/java/org/ExpenseTracker/config/WebConfig.java
@@ -1,17 +1,15 @@
 package org.ExpenseTracker.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableScheduling
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("*")
