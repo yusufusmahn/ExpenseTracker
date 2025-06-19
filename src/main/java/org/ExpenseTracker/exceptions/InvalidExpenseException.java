10e10621865aa210f6ef3b1229bb1b7e3cb14d27
yusufusman <yusuf.usman1003@gmail.com>
2025-06-19 11:41:48 +0100

    new task upload

--- src/main/java/org/ExpenseTracker/exceptions/InvalidExpenseException.java
+++
@@ -1,7 +0,0 @@
-package org.ExpenseTracker.exceptions;
-
-public class InvalidExpenseException extends ExpenseTracker {
-    public InvalidExpenseException(String message) {
-        super(message);
-    }
-}
